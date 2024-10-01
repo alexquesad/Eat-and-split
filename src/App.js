4f6613a32e6f067c1e9dc578eb06a307fb006b5f
alexquesad <alexandraquesad@gmail.com>
2024-10-01 14:20:56 +0200

    finished app static logic

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -77,6 +78,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the bill with X</h2>
+
+      <label>💸 Bill Value</label>
+      <input type="text" />
+
+      <label>🙋Your expense</label>
+      <input type="text" />
+
+      <label>👫 X's expense</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
